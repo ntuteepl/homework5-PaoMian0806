883033edfaa3a642e83731f1eef5374744532a1c
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-10-31 14:13:00 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -4,7 +4,7 @@
 int main()
 {
     /*
-    min
+    min 6
     hour h 30 0.5
     */
     int h,m;
@@ -14,23 +14,47 @@ int main()
     ang_m = 6*m;
     float ang1 = ang_h - ang_m;
     float ang2 = ang_m - ang_h;
-    if (ang1 > 180){
-        ang1-=180;
+    if (ang1 >= 180){
+        ang1 -= 180;
     }
-    else if (ang1 < -180){
-        ang1+=360;
+    if (ang2 >= 180){
+        ang2 -= 180;
     }
-    if (ang2 > 180){
-        ang2-=180;
+    if (ang1 > 0 && ang2 > 0){
+        if (ang1 < ang2 && ang2 < 180){
+            printf(".3f",ang2);
+        }
+        else {
+            printf(".3f",ang1);
+        }
     }
-    else if (ang2 < -180){
-        ang2+=360;
+    else if (ang1 < 0 && ang2 < 0){
+        ang1 += 360;
+        ang2 += 360;
+        if (ang1 < ang2 && ang2 < 180){
+            printf("%.3f",ang2);
+        }
+        else {
+            printf("%.3f",ang1);
+        }
     }
-    if (ang1 < ang2 || ang2 < 0){
-        printf("%.3f",ang1);
+    else if (ang1 < 0){
+        ang1 += 360;
+        if (ang1 < ang2 && ang2 < 180){
+            printf("%.3f",ang2);
+        }
+        else {
+            printf("%.3f",ang1);
+        }
     }
-    else {
-        printf("%.3f",ang2);
+    else if (ang2 < 0){
+        ang2 += 360;
+        if (ang1 < ang2 && ang2 < 180){
+            printf("%.3f",ang2);
+        }
+        else {
+            printf("%.3f",ang1);
+        }
     }
     return 0;
 }
