5610f917201e470b4bfbadbd9e40f1b92ed66d62
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-10-31 13:37:54 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int time,salary;
+    float money = 0;
+    scanf("%d%d",&time,&salary);
+    if (time >= 121){
+        money += (time - 120)*1.66*salary;
+        time = 120;
+    }
+    if (time >= 61){
+        money += (time - 60)*1.33*salary;
+        time = 60;
+    }
+    money += time*salary;
+    printf("%.1f",money);
+    return 0;
+}
