c042974ced30719f2595fb0753fbe1e01d0afc24
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-10-31 14:37:04 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -13,52 +13,18 @@ int main()
     ang_h = 30*h + 0.5*m;
     ang_m = 6*m;
     float ang1 = ang_h - ang_m;
-    float ang2 = ang_m - ang_h;
-    if (ang1 > 0 && ang2 > 0){
-        if (ang1 >= 180){
-            ang1 -= 180;
-        }
-        if (ang2 >= 180){
-            ang2 -= 180;
-        }
-        if (ang1 < ang2){
-            printf(".3f",ang2);
-        }
-        else {
-            printf(".3f",ang1);
-        }
+    if (ang1 < 0){
+        ang1 *= -1;
     }
-    else if (ang1 < 0 && ang2 < 0){
-        if (ang1 <= -180){
-            ang1 += 360;
-        }
-        if (ang1 < 0){
-            ang1 += 180;
-        }
-        if (ang2 <= -180){
-            ang2 += 360;
-        }
-        if (ang2 < 0){
-            ang2 += 180;
-        }
-        if (ang1 < ang2){
-            printf("%.3f",ang2);
-        }
-        else {
-            printf("%.3f",ang1);
-        }
+    float ang2 = 360 - ang1;
+    if (ang2 < 0){
+        ang2 *= -1;
     }
-    else if (ang1 < 0){
-        if (ang2 >= 180){
-            ang2 -= 180;
-        }
-        printf("%.3f",ang2);
-    }
-    else if (ang2 < 0){
-        if (ang1 >= 180){
-            ang1 -= 180;
-        }
+    if (ang1 < ang2){
         printf("%.3f",ang1);
+    }
+    else {
+        printf("%.3f",ang2);
     }
     return 0;
 }
