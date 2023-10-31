aed1f1e344923b7eac8c524cb5b5dab76f6bde58
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-10-31 13:53:10 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    min
+    hour h 30 0.5
+    */
+    int h,m;
+    float ang_h,ang_m;
+    scanf("%d%d",&h,&m);
+    ang_h = 30*h + 0.5*m;
+    ang_m = 6*m;
+    float ang = ang_h - ang_m;
+    if (ang > 180){
+        printf("%.3f",ang-180);
+    }
+    else if (ang < -180){
+        printf("%.3f",ang+360);
+    }
+    else {
+        printf("%.3f",ang);
+    }
+    return 0;
+}
 
