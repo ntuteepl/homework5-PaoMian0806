d2400709121da10123a52efe6fe94e0dbc719723
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-10-31 13:32:30 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int time;
+    float money;
+    scanf("%d",&time);
+    if (time >= 1500) {
+        money = time*0.9*0.79+0.05;
+    }
+    else if (time >= 800){
+        money = time*0.9*0.9+0.05;
+    }
+    else{
+        money = time*0.9;
+    }
+    printf("%.1f",money);
+    return 0;
+}
 
